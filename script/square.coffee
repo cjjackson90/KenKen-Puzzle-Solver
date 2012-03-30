e0497acc12638199f476b5c925d5b55e27e9a697
Colin Jackson <cj@CJ-Laptop.(none)>
2012-03-30 14:31:45 +0100

    Added row_id_char to class allowing easy way to get the row as its cell reference.

--- script/square.coffee
+++ script/square.coffee
@@ -4,6 +4,7 @@ class Square
 	target_op: null
 	value: null
 	row_id: null
+	row_id_char: null
 	column_id: null
 	cage_id: null
 
@@ -11,7 +12,8 @@ class Square
 	constructor: (row_id, column_id) ->
 		@row_id = row_id
 		@column_id = column_id
-		@id = (String.fromCharCode(row_id+65)) + (column_id+1) 
+		@row_id_char = String.fromCharCode(row_id+65)
+		@id = (@row_id_char) + (column_id+1) 
 		#console.log(@id)
 	
 	set_cage_id: (cage_id) ->
--- script/square.js
+++ script/square.js
@@ -5,12 +5,14 @@
     Square.prototype.target_op = null;
     Square.prototype.value = null;
     Square.prototype.row_id = null;
+    Square.prototype.row_id_char = null;
     Square.prototype.column_id = null;
     Square.prototype.cage_id = null;
     function Square(row_id, column_id) {
       this.row_id = row_id;
       this.column_id = column_id;
-      this.id = (String.fromCharCode(row_id + 65)) + (column_id + 1);
+      this.row_id_char = String.fromCharCode(row_id + 65);
+      this.id = this.row_id_char + (column_id + 1);
     }
     Square.prototype.set_cage_id = function(cage_id) {
       return this.cage_id = cage_id;
