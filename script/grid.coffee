69016edc91a7e6363fbf5f3fb603eff8455605b0
Colin Jackson <cjjackson90@gmail.com>
2012-04-03 20:51:45 +0100

    Corrected grid checking functions - row/column/cages are correct.

--- script/grid.coffee
+++ script/grid.coffee
@@ -52,8 +52,96 @@ class Grid
 				return false
 			return valid
 
+	check_rows_are_valid: () ->
+		cmp = [1..@size]
+		valid = true
+		for row in [0...@size]
+			current_row = []
+			for element in [0...@size]
+				current_row.push @display[row][element].value
+
+			sorted = current_row.sort(@ascending)
+
+			for i in [0...sorted.length]
+				if sorted[i] isnt cmp[i]
+					return false
+				# console.log "won't see this..."
+				# valid = true
+		return valid
+
+	check_cols_are_valid: ->
+		cmp = [1..@size]
+		valid = true
+		for col in [0...@size]
+			current_col = []
+			for element in [0...@size]
+				current_col.push @display[element][col].value
+			
+			sorted = current_col.sort(@ascending)
+			for i in [0...sorted.length]
+				if sorted[i] isnt cmp[i]
+					return false
+			# if current_col.sort is cmp
+			# 	valid = true
+			# else
+			# 	return false
+		return valid
+
+	check_cages_are_valid: ->
+		for cage in @cages
+			cage_nums = []
+			for i in cage.location
+				cage_nums.push i.value
+
+			switch cage.operation
+				when "+"
+					# sorted = cage_nums.sort(@ascending)
+					cage_value = 0
+					for i in cage_nums
+						cage_value += i
+					if cage_value isnt cage.target
+						return false
+				when "-"
+					sorted = cage_nums.sort(@descending)
+					cage_value = sorted[0]
+					for i in sorted[1...sorted.size]
+						cage_value -= i
+					if cage_value isnt cage.target
+						return false
+				when "*"
+					# sorted = cage_nums.sort(@ascending)
+					cage_value = 1
+					for i in cage_nums
+						cage_value *= i
+					if cage_value isnt cage.target
+						return false
+				when "/"
+					sorted = cage_nums.sort(@descending)
+					cage_value = sorted[0]
+					for i in sorted[1...sorted.size]
+						cage_value /= i
+					if cage_value isnt cage.target
+						return false
+				# when "-"
+				# 	current_cage = []
+				# 	for i in cage.location
+				# 		current_cage.push i.value
+				# 	current_cage.sort
+				# 	for i in [current_cage.le]
+				# 	for i in cage.location
+				# 		cage_value -= i.value 
+				# when "*"
+
+				# when "/"
+
+			# return false if cage_value isnt cage.target 
+		return true		
+
+
+
+
 	cages_have_candidates: () ->
-		for cage in cages
+		for cage in @cages
 			if cage.candidates is null
 				return false
 		return true
@@ -65,5 +153,13 @@ class Grid
 		column = square_id.substring(1) - 1 #Makes index of column one = 0, etc 
 		#console.log(row + " "+ column)
 		return @display[row][column]
+
+	# These two functions allow the js sort method to sort numerically
+	# as opposed to lexicographically
+	descending: (a,b) ->
+		return b-a
+
+	ascending: (a,b) ->
+		return a-b
 
 this.Grid = Grid
--- script/grid.js
+++ script/grid.js
@@ -55,10 +55,114 @@
         return valid;
       }
     };
+    Grid.prototype.check_rows_are_valid = function() {
+      var cmp, current_row, element, i, row, sorted, valid, _i, _ref, _ref2, _ref3, _ref4, _results;
+      cmp = (function() {
+        _results = [];
+        for (var _i = 1, _ref = this.size; 1 <= _ref ? _i <= _ref : _i >= _ref; 1 <= _ref ? _i++ : _i--){ _results.push(_i); }
+        return _results;
+      }).apply(this, arguments);
+      valid = true;
+      for (row = 0, _ref2 = this.size; 0 <= _ref2 ? row < _ref2 : row > _ref2; 0 <= _ref2 ? row++ : row--) {
+        current_row = [];
+        for (element = 0, _ref3 = this.size; 0 <= _ref3 ? element < _ref3 : element > _ref3; 0 <= _ref3 ? element++ : element--) {
+          current_row.push(this.display[row][element].value);
+        }
+        sorted = current_row.sort(this.ascending);
+        for (i = 0, _ref4 = sorted.length; 0 <= _ref4 ? i < _ref4 : i > _ref4; 0 <= _ref4 ? i++ : i--) {
+          if (sorted[i] !== cmp[i]) {
+            return false;
+          }
+        }
+      }
+      return valid;
+    };
+    Grid.prototype.check_cols_are_valid = function() {
+      var cmp, col, current_col, element, i, sorted, valid, _i, _ref, _ref2, _ref3, _ref4, _results;
+      cmp = (function() {
+        _results = [];
+        for (var _i = 1, _ref = this.size; 1 <= _ref ? _i <= _ref : _i >= _ref; 1 <= _ref ? _i++ : _i--){ _results.push(_i); }
+        return _results;
+      }).apply(this, arguments);
+      valid = true;
+      for (col = 0, _ref2 = this.size; 0 <= _ref2 ? col < _ref2 : col > _ref2; 0 <= _ref2 ? col++ : col--) {
+        current_col = [];
+        for (element = 0, _ref3 = this.size; 0 <= _ref3 ? element < _ref3 : element > _ref3; 0 <= _ref3 ? element++ : element--) {
+          current_col.push(this.display[element][col].value);
+        }
+        sorted = current_col.sort(this.ascending);
+        for (i = 0, _ref4 = sorted.length; 0 <= _ref4 ? i < _ref4 : i > _ref4; 0 <= _ref4 ? i++ : i--) {
+          if (sorted[i] !== cmp[i]) {
+            return false;
+          }
+        }
+      }
+      return valid;
+    };
+    Grid.prototype.check_cages_are_valid = function() {
+      var cage, cage_nums, cage_value, i, sorted, _i, _j, _k, _l, _len, _len2, _len3, _len4, _len5, _len6, _m, _n, _ref, _ref2, _ref3, _ref4;
+      _ref = this.cages;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        cage = _ref[_i];
+        cage_nums = [];
+        _ref2 = cage.location;
+        for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+          i = _ref2[_j];
+          cage_nums.push(i.value);
+        }
+        switch (cage.operation) {
+          case "+":
+            cage_value = 0;
+            for (_k = 0, _len3 = cage_nums.length; _k < _len3; _k++) {
+              i = cage_nums[_k];
+              cage_value += i;
+            }
+            if (cage_value !== cage.target) {
+              return false;
+            }
+            break;
+          case "-":
+            sorted = cage_nums.sort(this.descending);
+            cage_value = sorted[0];
+            _ref3 = sorted.slice(1, sorted.size);
+            for (_l = 0, _len4 = _ref3.length; _l < _len4; _l++) {
+              i = _ref3[_l];
+              cage_value -= i;
+            }
+            if (cage_value !== cage.target) {
+              return false;
+            }
+            break;
+          case "*":
+            cage_value = 1;
+            for (_m = 0, _len5 = cage_nums.length; _m < _len5; _m++) {
+              i = cage_nums[_m];
+              cage_value *= i;
+            }
+            if (cage_value !== cage.target) {
+              return false;
+            }
+            break;
+          case "/":
+            sorted = cage_nums.sort(this.descending);
+            cage_value = sorted[0];
+            _ref4 = sorted.slice(1, sorted.size);
+            for (_n = 0, _len6 = _ref4.length; _n < _len6; _n++) {
+              i = _ref4[_n];
+              cage_value /= i;
+            }
+            if (cage_value !== cage.target) {
+              return false;
+            }
+        }
+      }
+      return true;
+    };
     Grid.prototype.cages_have_candidates = function() {
-      var cage, _i, _len;
-      for (_i = 0, _len = cages.length; _i < _len; _i++) {
-        cage = cages[_i];
+      var cage, _i, _len, _ref;
+      _ref = this.cages;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        cage = _ref[_i];
         if (cage.candidates === null) {
           return false;
         }
@@ -70,6 +174,12 @@
       row = square_id.substring(0, 1).charCodeAt(0) - 65;
       column = square_id.substring(1) - 1;
       return this.display[row][column];
+    };
+    Grid.prototype.descending = function(a, b) {
+      return b - a;
+    };
+    Grid.prototype.ascending = function(a, b) {
+      return a - b;
     };
     return Grid;
   })();
