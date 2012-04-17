1eb57fca7b70c947ad92798d36439f28d120b98c
Colin Jackson <cjjackson90@gmail.com>
2012-04-17 23:43:59 +0100

    Added main functionality to the solve class.
     + Solve method now implemented and using recursion to backtrack solutions.
     + get_potentials method implemented which creates new grid instances which the current grid can
    	branch from. These grids contain the a variation of a particular cage's candidates.

--- script/solver.coffee
+++ script/solver.coffee
@@ -6,32 +6,225 @@ class Solver
 	constructor: () ->
 		
 	solve: (grid) ->
-		if grid.verify_correct( ) is true
-			return valid
+		# console.log grid
+		grid_complete = true
+		for i in [0...grid.size]
+			for j in [0...grid.size]
+				if grid.display[i][j].value is null
+					grid_complete = false
+					break
+
+		solution = false
+		if grid.display[0][0].value is 3 and grid.display[0][1].value is 2 and grid.display[0][2].value is 1 and grid.display[1][0].value is 2 and grid.display[1][1].value is 1 and grid.display[1][2].value is 3 and grid.display[2][0].value is 1 and grid.display[2][1].value is 3 and grid.display[2][2].value is 2
+			solution = true
 		
-		if cages_have_candidates( ) is false
-			return invalid
+		verified_correct = grid.verify_correct()
+
+		if solution
+			console.log "$$$$$$$$$$$$$$$$$$$$$$$$$$"
+			console.log verified_correct
+
+		if verified_correct is false and grid_complete is true
+			return_obj =
+				status:"invalid"
+			return return_obj
+
+		if verified_correct is true
+			return_obj = 
+				status:"valid"
+				valid_grid: grid
+			return return_obj
+		
+		if grid.cages_have_candidates( ) is false
+			return_obj = 
+				status:"invalid"
+			return return_obj
+
 
 		# TODO: Implement strategies.
 
-		if backtrack(grid) is invalid
-			return invalid
-		else
-			return update
+		console.log "A1 = #{grid.display[0][0].value}"
+		console.log "A2 = #{grid.display[0][1].value}"
+		console.log "A3 = #{grid.display[0][2].value}"
+		console.log "B1 = #{grid.display[1][0].value}"
+		console.log "B2 = #{grid.display[1][1].value}"
+		console.log "B3 = #{grid.display[1][2].value}"
+		console.log "C1 = #{grid.display[2][0].value}"
+		console.log "C2 = #{grid.display[2][1].value}"
+		console.log "C3 = #{grid.display[2][2].value}"
 
-	backtrack: (grid) ->
-		profitable_cage # Chose some method of selecting a profitable cage for backtracking. Store id.
+		potentials = @get_potentials(grid)
+		if potentials.length is 1
+			return_obj =
+				status:"update"
+				updated_grid: potentials[0]
+			return return_obj
+
+		for new_grid in potentials
+			return_obj = @solve(new_grid)
+
+			switch return_obj.status
+				when "update"
+					return return_obj
+				when "valid"
+					return return_obj
+				when "invalid"
+					console.log "Breaking because this grid is invalid"
+					console.log "A1 = #{new_grid.display[0][0].value}"
+					console.log "A2 = #{new_grid.display[0][1].value}"
+					console.log "A3 = #{new_grid.display[0][2].value}"
+					console.log "B1 = #{new_grid.display[1][0].value}"
+					console.log "B2 = #{new_grid.display[1][1].value}"
+					console.log "B3 = #{new_grid.display[1][2].value}"
+					console.log "C1 = #{new_grid.display[2][0].value}"
+					console.log "C2 = #{new_grid.display[2][1].value}"
+					console.log "C3 = #{new_grid.display[2][2].value}"
+
+					break
+		return {status:"invalid"}			
+
+
+
+	# 	if @backtrack(grid) is "invalid"
+	# 		return {status:"invalid"}
+	# 	else
+	# 		return {status:"update"}
+
+	get_potentials: (grid) ->
+		profitable_cage = null # Chose some method of selecting a profitable cage for backtracking. Store id.
 		potentials = []
 
-		###for candidate in profitable_cage
-			temp = grid
-			temp.
-			potentials.push
-		###
+		smallest_cage = grid.cages[0]
+		first_index = 0
+
+		for i in [0...grid.cages.length]
+			console.log grid.cages[i].location
+			for sq in grid.cages[i].location
+				if sq.value is null
+					smallest_cage = grid.cages[i]
+					first_index = i
+					break
+
+		console.log "smallest_cage.id"
+		console.log smallest_cage.id
+		# smallest_cage = grid.cages[0]
+		# for square in grid.cages[0]
+			# if not square.value?
+				# viable = true
+				# break
+
+		for cand_group, candidate of smallest_cage.candidates
+			smallest_length = candidate.length
+
+		for cage in grid.cages[first_index+1...grid.cages.length]
+			# console.log cage
+			viable = false
+			for square in cage.location
+				if square.value is null
+					viable = true
+					break
+
+			for cand_group, candidate of cage.candidates
+				console.log "candidate.length = #{candidate.length}, smallest_length = #{smallest_length}, viable?=#{viable}"
+				if candidate.length < smallest_length and viable is true
+					smallest_cage = cage
+					smallest_length = candidate.length
+		# console.log "profitable_cage"
+		profitable_cage = smallest_cage #grid.cages[0] #smallest_cage
+
+		# console.log profitable_cage
+		# Populate potentials
+		for cand_group, cand of profitable_cage.candidates
+			for candidate in cand
+				# console.log candidate
+				temp = new Grid (grid.size)
+				jQuery.extend(true, temp, grid) #@clone(grid)
+				# temp = @clone(grid)
+				# temp = grid
+				for cage in temp.cages
+					for i in [0...cage.location.length]
+						# console.log cage.location[i]
+						# console.log temp.get_square(cage.location[i].id)
+						cage.location[i] = temp.get_square(cage.location[i].id)
+					if cage.id is profitable_cage.id
+						# console.log "updating square"
+						temp.add_candidate cage.location, candidate
+						# cage.add_candidate_to_grid candidate
+				# console.log "Temp is here"
+				# console.log temp
+				# console.log temp.cages
+				potentials.push @clone temp
+				temp = null
+
+
+		# console.log "potentials I..."
+		# console.log potentials
+		potentials
+
+
+
+	# backtrack: (grid) ->
+		
+	# 	for grid in potentials
+	# 		#for testing use - remove every other potential
+	# 		# i = 0
+	# 		if potentials.length is 1
+	# 			break
+
+	# 		# grid_status = solve (grid)
+	# 		if grid_status is "invalid"
+	# 			# console.log grid
+	# 			potentials = @remove potentials, grid
+	# 			# console.log "potentials I.V"
+	# 			# console.log potentials
+	# 			i++
+	# 		else if grid_status isnt "valid"
+	# 			solve(grid)
+
+	# 	if potentials.length is 1 then
+	# 		# update ( )
+	# 		# return "update"
+	# 	else
+	# 		return "invalid"
+
+	# 	# console.log "potentials II..."
+	# 	# console.log potentials
+
+
+
+	# 	# for all g in potentials
+	# 		#if solve(g) = invalid
+	# 			#remove g
+	# 		#else if solve(g) =/= valid
+	# 			#solve g
+
+	# 		# if potentials.length is 1
+	# 			#break
+	# 	# if potentials.length is 1 then
+	# 		#update info
+	# 		#return 'update'
+	# 	# else
+	# 		# return 'invalid'
+
+
+	# 	return "update"
 
 	append_solution_order: (fresh_vals) ->
 
 	update_puzzle_info: () ->
-		
+
+	clone: (obj) ->
+		if not obj? or typeof obj isnt 'object'
+			return obj
+
+		newInstance = new obj.constructor()
+
+		for key of obj
+			newInstance[key] = @clone obj[key]
+
+		return newInstance
+
+	remove: (arr, val) ->
+		x for x in arr when x!=val
 
 this.Solver = Solver
--- script/solver.js
+++ script/solver.js
@@ -5,29 +5,182 @@
     Solver.prototype.solution_order = [];
     function Solver() {}
     Solver.prototype.solve = function(grid) {
-      if (grid.verify_correct() === true) {
-        return valid;
+      var grid_complete, i, j, new_grid, potentials, return_obj, solution, verified_correct, _i, _len, _ref, _ref2;
+      grid_complete = true;
+      for (i = 0, _ref = grid.size; 0 <= _ref ? i < _ref : i > _ref; 0 <= _ref ? i++ : i--) {
+        for (j = 0, _ref2 = grid.size; 0 <= _ref2 ? j < _ref2 : j > _ref2; 0 <= _ref2 ? j++ : j--) {
+          if (grid.display[i][j].value === null) {
+            grid_complete = false;
+            break;
+          }
+        }
       }
-      if (cages_have_candidates() === false) {
-        return invalid;
+      solution = false;
+      if (grid.display[0][0].value === 3 && grid.display[0][1].value === 2 && grid.display[0][2].value === 1 && grid.display[1][0].value === 2 && grid.display[1][1].value === 1 && grid.display[1][2].value === 3 && grid.display[2][0].value === 1 && grid.display[2][1].value === 3 && grid.display[2][2].value === 2) {
+        solution = true;
       }
-      if (backtrack(grid) === invalid) {
-        return invalid;
-      } else {
-        return update;
+      verified_correct = grid.verify_correct();
+      if (solution) {
+        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        console.log(verified_correct);
       }
+      if (verified_correct === false && grid_complete === true) {
+        return_obj = {
+          status: "invalid"
+        };
+        return return_obj;
+      }
+      if (verified_correct === true) {
+        return_obj = {
+          status: "valid",
+          valid_grid: grid
+        };
+        return return_obj;
+      }
+      if (grid.cages_have_candidates() === false) {
+        return_obj = {
+          status: "invalid"
+        };
+        return return_obj;
+      }
+      console.log("A1 = " + grid.display[0][0].value);
+      console.log("A2 = " + grid.display[0][1].value);
+      console.log("A3 = " + grid.display[0][2].value);
+      console.log("B1 = " + grid.display[1][0].value);
+      console.log("B2 = " + grid.display[1][1].value);
+      console.log("B3 = " + grid.display[1][2].value);
+      console.log("C1 = " + grid.display[2][0].value);
+      console.log("C2 = " + grid.display[2][1].value);
+      console.log("C3 = " + grid.display[2][2].value);
+      potentials = this.get_potentials(grid);
+      if (potentials.length === 1) {
+        return_obj = {
+          status: "update",
+          updated_grid: potentials[0]
+        };
+        return return_obj;
+      }
+      for (_i = 0, _len = potentials.length; _i < _len; _i++) {
+        new_grid = potentials[_i];
+        return_obj = this.solve(new_grid);
+        switch (return_obj.status) {
+          case "update":
+            return return_obj;
+          case "valid":
+            return return_obj;
+          case "invalid":
+            console.log("Breaking because this grid is invalid");
+            console.log("A1 = " + new_grid.display[0][0].value);
+            console.log("A2 = " + new_grid.display[0][1].value);
+            console.log("A3 = " + new_grid.display[0][2].value);
+            console.log("B1 = " + new_grid.display[1][0].value);
+            console.log("B2 = " + new_grid.display[1][1].value);
+            console.log("B3 = " + new_grid.display[1][2].value);
+            console.log("C1 = " + new_grid.display[2][0].value);
+            console.log("C2 = " + new_grid.display[2][1].value);
+            console.log("C3 = " + new_grid.display[2][2].value);
+            break;
+        }
+      }
+      return {
+        status: "invalid"
+      };
     };
-    Solver.prototype.backtrack = function(grid) {
-      profitable_cage;      var potentials;
-      return potentials = [];
-      /*for candidate in profitable_cage
-      			temp = grid
-      			temp.
-      			potentials.push
-      		*/
+    Solver.prototype.get_potentials = function(grid) {
+      var cage, cand, cand_group, candidate, first_index, i, potentials, profitable_cage, smallest_cage, smallest_length, sq, square, temp, viable, _i, _j, _k, _l, _len, _len2, _len3, _len4, _len5, _m, _ref, _ref2, _ref3, _ref4, _ref5, _ref6, _ref7, _ref8, _ref9;
+      profitable_cage = null;
+      potentials = [];
+      smallest_cage = grid.cages[0];
+      first_index = 0;
+      for (i = 0, _ref = grid.cages.length; 0 <= _ref ? i < _ref : i > _ref; 0 <= _ref ? i++ : i--) {
+        console.log(grid.cages[i].location);
+        _ref2 = grid.cages[i].location;
+        for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
+          sq = _ref2[_i];
+          if (sq.value === null) {
+            smallest_cage = grid.cages[i];
+            first_index = i;
+            break;
+          }
+        }
+      }
+      console.log("smallest_cage.id");
+      console.log(smallest_cage.id);
+      _ref3 = smallest_cage.candidates;
+      for (cand_group in _ref3) {
+        candidate = _ref3[cand_group];
+        smallest_length = candidate.length;
+      }
+      _ref4 = grid.cages.slice(first_index + 1, grid.cages.length);
+      for (_j = 0, _len2 = _ref4.length; _j < _len2; _j++) {
+        cage = _ref4[_j];
+        viable = false;
+        _ref5 = cage.location;
+        for (_k = 0, _len3 = _ref5.length; _k < _len3; _k++) {
+          square = _ref5[_k];
+          if (square.value === null) {
+            viable = true;
+            break;
+          }
+        }
+        _ref6 = cage.candidates;
+        for (cand_group in _ref6) {
+          candidate = _ref6[cand_group];
+          console.log("candidate.length = " + candidate.length + ", smallest_length = " + smallest_length + ", viable?=" + viable);
+          if (candidate.length < smallest_length && viable === true) {
+            smallest_cage = cage;
+            smallest_length = candidate.length;
+          }
+        }
+      }
+      profitable_cage = smallest_cage;
+      _ref7 = profitable_cage.candidates;
+      for (cand_group in _ref7) {
+        cand = _ref7[cand_group];
+        for (_l = 0, _len4 = cand.length; _l < _len4; _l++) {
+          candidate = cand[_l];
+          temp = new Grid(grid.size);
+          jQuery.extend(true, temp, grid);
+          _ref8 = temp.cages;
+          for (_m = 0, _len5 = _ref8.length; _m < _len5; _m++) {
+            cage = _ref8[_m];
+            for (i = 0, _ref9 = cage.location.length; 0 <= _ref9 ? i < _ref9 : i > _ref9; 0 <= _ref9 ? i++ : i--) {
+              cage.location[i] = temp.get_square(cage.location[i].id);
+            }
+            if (cage.id === profitable_cage.id) {
+              temp.add_candidate(cage.location, candidate);
+            }
+          }
+          potentials.push(this.clone(temp));
+          temp = null;
+        }
+      }
+      return potentials;
     };
     Solver.prototype.append_solution_order = function(fresh_vals) {};
     Solver.prototype.update_puzzle_info = function() {};
+    Solver.prototype.clone = function(obj) {
+      var key, newInstance;
+      if (!(obj != null) || typeof obj !== 'object') {
+        return obj;
+      }
+      newInstance = new obj.constructor();
+      for (key in obj) {
+        newInstance[key] = this.clone(obj[key]);
+      }
+      return newInstance;
+    };
+    Solver.prototype.remove = function(arr, val) {
+      var x, _i, _len, _results;
+      _results = [];
+      for (_i = 0, _len = arr.length; _i < _len; _i++) {
+        x = arr[_i];
+        if (x !== val) {
+          _results.push(x);
+        }
+      }
+      return _results;
+    };
     return Solver;
   })();
   this.Solver = Solver;
