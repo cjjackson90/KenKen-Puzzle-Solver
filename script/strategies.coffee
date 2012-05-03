62496f30296b9076dc6e0b6a89236f2646a0a263
Colin Jackson <cjjackson90@gmail.com>
2012-05-03 23:45:43 +0100

    Added first strategy - one candidate. Finds all cages that only have one possible solution and adds them to the grid, whilst populating an array of updated squares to update the candidates of affected squares.

--- script/strategies.coffee
+++ script/strategies.coffee
@@ -1,12 +1,56 @@
 class Strategies
 
-	one_candidate: () ->
+	constructor: () ->
+
+	one_candidate: (grid) ->
+		vals_added = false
+		vals = []
+		for cage in grid.cages
+			if cage.candidates.length is 1
+				valid = false
+				for sq in cage.location
+					if sq.value is null
+						valid = true
+						break
+				if valid
+					cage.add_candidate_to_grid( cage.candidates[0] )
+					for sq in cage.location
+						vals.push sq
+					vals_added = true
+		return_obj =
+			status: vals_added
+			vals: vals
+			grid: grid
+		return return_obj
+			# for candidate in cage.candidates
+
+		# vals_added = false
+		# for cage in grid.cages
+		# 	console.log "current_cage"
+		# 	console.log cage
+		# 	for cand_group, cand of cage.candidates
+		# 		console.log "cand_group.length = #{cand_group.length}"
+		# 		console.log "cand.length = #{cand.length}"
+		# 		if cand.length is 1
+		# 			valid = false
+		# 			console.log cage.location
+		# 			for sq in cage.location
+		# 				if sq.value is null
+		# 					valid = true
+		# 					break
+		# 			if valid
+		# 				console.log "value is null; valid sq"
+		# 				cage.add_candidate_to_grid( cand )
+		# 				vals_added = true
+		# 		# for i in [0...candidates[0].length]
+		# 			# set_value( candidates[0][i] )
+		# return vals_added
 		return false
 
-	common_numbers: () ->
+	common_numbers: (grid) ->
 		return false
 
-	certain_vals: () ->
+	certain_vals: (grid) ->
 		return false
 
 this.Strategies = Strategies
--- script/strategies.js
+++ script/strategies.js
@@ -2,13 +2,46 @@
   var Strategies;
   Strategies = (function() {
     function Strategies() {}
-    Strategies.prototype.one_candidate = function() {
+    Strategies.prototype.one_candidate = function(grid) {
+      var cage, return_obj, sq, valid, vals, vals_added, _i, _j, _k, _len, _len2, _len3, _ref, _ref2, _ref3;
+      vals_added = false;
+      vals = [];
+      _ref = grid.cages;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        cage = _ref[_i];
+        if (cage.candidates.length === 1) {
+          valid = false;
+          _ref2 = cage.location;
+          for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+            sq = _ref2[_j];
+            if (sq.value === null) {
+              valid = true;
+              break;
+            }
+          }
+          if (valid) {
+            cage.add_candidate_to_grid(cage.candidates[0]);
+            _ref3 = cage.location;
+            for (_k = 0, _len3 = _ref3.length; _k < _len3; _k++) {
+              sq = _ref3[_k];
+              vals.push(sq);
+            }
+            vals_added = true;
+          }
+        }
+      }
+      return_obj = {
+        status: vals_added,
+        vals: vals,
+        grid: grid
+      };
+      return return_obj;
       return false;
     };
-    Strategies.prototype.common_numbers = function() {
+    Strategies.prototype.common_numbers = function(grid) {
       return false;
     };
-    Strategies.prototype.certain_vals = function() {
+    Strategies.prototype.certain_vals = function(grid) {
       return false;
     };
     return Strategies;
